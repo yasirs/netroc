cb344ffd90f70e9dd4fb59e20bf5e1e4aef1e26f
yasirs <yasir.suhail@gmail.com>
2011-09-15 12:05:12 -0300

    legends in plot

--- examples/plotting.py
+++ examples/plotting.py
@@ -5,11 +5,16 @@ def plotComparison(trainfile,testfile,scorelist,labelList=None,method="roc"):
 	nr = netroc.NetReader(trainfile,testfile)
 	if labelList!=None:
 		labels = labelList
+	else:
+		labels = []
 	for scorefile in scorelist:
 		scores = nr.makeScores(scorefile)
 		d = yard.BinaryClassifierData(zip(scores,nr.getLabels()))
 		if (method.upper()=='ROC'):
 			result = yard.PrecisionRecallCurve(d)
 		pylab.plot(map(lambda x:x[0],result.points),map(lambda x:x[1],result.points))
-	pylab.legend()
+		if labelList==None:
+			labels.append(scorefile)
+	pylab.legend(labels)
+
 
