2af27fac3acead0d14a20cbb82c5458e6077d09f
Yasir Suhail <yasir.suhail@gmail.com>
2011-09-14 21:31:50 -0400

    plotting, no complete

---
+++ examples/plotting.py
@@ -0,0 +1,15 @@
+import netroc
+import yard, pylab
+
+def plotComparison(trainfile,testfile,scorelist,labelList=None,method="roc"):
+	nr = netroc.NetReader(trainfile,testfile)
+	if labelList!=None:
+		labels = labelList
+	for scorefile in scorelist:
+		scores = nr.makeScores(scorefile)
+		d = yard.BinaryClassifierData(zip(scores,nr.getLabels()))
+		if (method.upper()=='ROC'):
+			result = yard.PrecisionRecallCurve(d)
+		pylab.plot(map(lambda x:x[0],result.points),map(lambda x:x[1],result.points))
+	pylab.legend()
+
