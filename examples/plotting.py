4f72d23b928567065dd16c9558e1f84b443850c1
Yasir Suhail <yasir.suhail@gmail.com>
2011-09-16 13:18:34 -0400

    roc and pr

--- examples/plotting.py
+++ examples/plotting.py
@@ -11,6 +11,8 @@ def plotComparison(trainfile,testfile,scorelist,labelList=None,method="roc"):
 		scores = nr.makeScores(scorefile)
 		d = yard.BinaryClassifierData(zip(scores,nr.getLabels()))
 		if (method.upper()=='ROC'):
+			result = yard.ROCCurve(d)
+		else:
 			result = yard.PrecisionRecallCurve(d)
 		pylab.plot(map(lambda x:x[0],result.points),map(lambda x:x[1],result.points))
 		if labelList==None:
